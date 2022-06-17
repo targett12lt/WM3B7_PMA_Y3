0bed879a736887b22ec6d31c20c8f0d2eedce6f3
ltargett <ltargett@jaguarlandrover.com>
2022-06-17 10:26:32 +0100

    Creating base script to call modules/packages from & updating .gitignore to hide .pyc files

--- src/U1943174_SentimentAnalysis.py
+++ src/U1943174_SentimentAnalysis.py
@@ -1,2 +1,23 @@
 import common
 import models
+
+import os
+# import pandas as pd
+
+
+# Getting file path to data:
+os.chdir('....')  # Changing the working directory from script directory
+cwd = os.getcwd()
+base_folder_data = os.path.join(cwd, r'data\aclImdb')  # Specifying base data directory
+
+# Setting folders for training and testing data:
+train_data = os.path.join(base_folder_data, 'train')
+test_data = os.path.join(base_folder_data, 'test')
+
+# print('base_folder_data:', base_folder_data)  # FOR DEBUGGING PURPOSES
+
+# Creating Pandas DF:
+# training_df = pd.DataFrame(columns=['Review', 'PositiveReview', 'NegativeReview', 'Sentiment'])
+
+training_data = common.importData(train_data)
+print(training_data)
