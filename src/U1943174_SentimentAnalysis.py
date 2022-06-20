fc7ebadb362eedcb3f255b913b1aaa98e00219c4
ltargett <ltargett@jaguarlandrover.com>
2022-06-20 16:05:16 +0100

    Implemented 'BagOfWords' algorithm, outputs documentVectors and a bag of words

--- src/U1943174_SentimentAnalysis.py
+++ src/U1943174_SentimentAnalysis.py
@@ -2,8 +2,6 @@ import common
 import models
 
 import os
-import pandas as pd
-
 
 # Getting file path to data:
 os.chdir('....')  # Changing the working directory from script directory
@@ -16,8 +14,20 @@ test_data = os.path.join(base_folder_data, 'test')
 
 # print('base_folder_data:', base_folder_data)  # FOR DEBUGGING PURPOSES
 
-# Creating Pandas DF:
-# training_df = pd.DataFrame(columns=['Review', 'PositiveReview', 'NegativeReview', 'Sentiment'])
+# If running from scratch and creating PKL:
+# training_data = common.importData(train_data)
+# common.save_to_pkl(training_data, 'PreProcessedTrainingData.pkl')  # Saving data to pkl so it can be opened quickly
 
-training_data = common.importData(train_data)
-print(training_data.head(30))
+# Importing data from PKL file to make debugging/development quicker:
+training_data = common.read_from_pkl('PreProcessedTrainingData.pkl')
+
+# print(training_data.head(30))  # FOR DEBUGGING PURPOSES
+
+# TF-IDF Approach:
+# TF_IDF_DataFrame = common.tf_idf(training_data['Review'])
+
+stitched_strings = training_data['Review'].apply(lambda x: ' '.join([word for word in x]))
+bag_of_words, documentVectors = common.bagOfWords(stitched_strings)
+print(bag_of_words)
+print(documentVectors)
+
--- src/common/feature_engineering.py
+++ src/common/feature_engineering.py
@@ -1,3 +1,22 @@
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
+
+
+
+from statistics import mean
+import pandas as pd
+
+
+import nltk
+from nltk.sentiment import SentimentAnalyzer
+
+sia = SentimentAnalyzer()
+
+
+# positive_words = [word for word,
+#     nltk.pos_tag(nltk.corpus.movie_reviews.words(categories=["pos"]))
+# )]
+# positive_fd = nltk.FreqDist(positive_words)
+# top_100_positive = {word for word, count in positive_fd.most_common(100)}
 
 
 # Feature engineering = process of creating features for the ML models to use for classification models
@@ -8,13 +27,43 @@
 # *Bag of words approach - a way of extracting features from text for use in modelling, such as ML Algorthms
 #   - It describes the occurence of words in a document
 
-def tf_idf():
+def tf_idf(dataset):
     'Utilises the TF-IDF Feature Engineering approach'
-    print('Hello world')
+    print('dataset:', dataset)
+    tfIdfVectorizer= TfidfVectorizer(use_idf=True)
+    for review in dataset:
+        tfIdf = tfIdfVectorizer.fit_transform(review)
+        df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+        df = df.sort_values('TF-IDF', ascending=False)
+    print(df.head(25))
+    print(tfIdf.shape)
+    print(tfIdfVectorizer.get_feature_names_out())
+    print(len(tfIdfVectorizer.get_feature_names_out()))
 
-def tf():
-    'Utilises the term frequency (TF) - weighting method'
-    print('Hello from the normal term frequency method')
+    # tf_idf_transformer = TfidfTransformer(use_idf=True)
+    # count_vectorizer = CountVectorizer()
+    
+    # wordCount = count_vectorizer.fit_transform(dataset)
+    # new_tf_idf = tf_idf_transformer.fit_transform(wordCount)
+    # df = pd.DataFrame(new_tf_idf[0].T.todense(), index=count_vectorizer.get_feature_names(), columns=["TF-IDF"])
+    # df = df.sort_values('TF-IDF', ascending=False)
+    # print (df.head(25))
+
+
+
+def tf(bagOfWords, ):
+    '''Utilises the term frequency (TF) - weighting method
+    
+    Inputs: 
+    * 'bagOfWords' - Bag of Words output from function
+    * 'NumberOfWords' - The number of unique words in the 'document'
+
+    Outputs:
+    * Dictionary containing term frequency
+
+    '''
+    tf_Dictionary = {}
+    Length_BagOfWords = len(bagOfWords)
 
 def n_gram(n_value):
     'Allows the user to be able to input the n-value'
@@ -23,8 +72,18 @@ def FrequencyDistribution():
     '''Utilises nltk's frequency distribution function'''
     print("Hi from the FD Function")
 
-def bagOfWords():
+def bagOfWords(documents):
     '''Utilises SkLean's Count Vectoriser function to implement
-    a "bag of words" method'''
-    print('Debugging')
+    a "bag of words" method
+    
+    Function assumes all sentances have been tokenized as part of preprocessing
+    '''
+    vectorizer = CountVectorizer()  # Initializing 'CountVectorizer'
+    x = vectorizer.fit_transform(documents.values)
 
+    # print(vectorizer.get_feature_names())  # Printing 'feature_names' for dataset
+    # print(x.toarray())  # Creating an array based upon 'CountVectorizer' for dataset
+    
+    return vectorizer.get_feature_names(), x.toarray()
+
+    # documents['']
