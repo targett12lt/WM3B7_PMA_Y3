852ed5764c1bc248c323f3ff03cedb8f0aba1b46
ltargett <ltargett@jaguarlandrover.com>
2022-06-27 09:59:50 +0100

    Added visualisation module to 'common' package

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,2 +1,4 @@
 from .pre_processing import *
 from .feature_engineering import *
+from .visualisation import *
+
---
+++ src/common/visualisation.py
@@ -0,0 +1,7 @@
+import seaborn as sns
+import matplotlib as plt
+
+# Space to visualise term frequency, etc.
+# compare negative vs positive sentiment in graph for basic data exploration/analysis
+
+
