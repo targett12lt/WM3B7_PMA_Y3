8da4728a909fb8c2088ec2d670de379408373bf9
ltargett <ltargett@jaguarlandrover.com>
2022-06-27 11:29:17 +0100

    Added basic visualisation of sentiment for training dataset

--- src/U1943174_SentimentAnalysis.py
+++ src/U1943174_SentimentAnalysis.py
@@ -14,34 +14,34 @@ test_data = os.path.join(base_folder_data, 'test')
 
 # print('base_folder_data:', base_folder_data)  # FOR DEBUGGING PURPOSES
 
+################################ IMPORTING DATA ################################
+
 # If running from scratch and creating PKL:
-training_data = common.importData(train_data)
-common.save_to_pkl(training_data, 'TrainingData.pkl')  # Saving data to pkl so it can be opened quickly
+# training_data = common.importData(train_data)
+# common.save_to_pkl(training_data, 'TrainingData.pkl')  # Saving data to pkl so it can be opened quickly
 
 # Importing data from PKL file to make debugging/development quicker:
-# training_data = common.read_from_pkl('TrainingData.pkl')
+training_data = common.read_from_pkl('TrainingData.pkl')
 
-# print(training_data.head(30))  # FOR DEBUGGING PURPOSES
-
-# TF-IDF Approach:
-# TF_IDF_DataFrame = common.tf_idf(training_data['Review'])
-
-# stitched_strings = training_data['Review'].apply(lambda x: ' '.join([word for word in x]))
-# word_list, bagOfWordsVector = common.bagOfWords(stitched_strings)
-# print(word_list)
-# print(bagOfWordsVector)
+################################ CLEANING DATA #################################
 
 common.add_cleaned_column(training_data, 'Review', 'CleanedReview')
 
 print('Cleaned Pandas DF:')
-
 print(training_data)
 
-bag_of_words = common.tf(training_data)
+############################### DATA EXPLORATION ###############################
 
-common.FrequencyDistribution(training_data['CleanedReview'])
+# NEED TO PUT SOME DATA EXPLORATION/VISUALISATION HERE
+common.visualise_sentiment_type(training_data)
 
-common.tf_idf(bag_of_words)  # Uses previous bag of words to avoid duplication of process (resulting in poorer compuational performance)
+############################## FEATURE ENGINEERING #############################
+
+bag_of_words = common.BagOfWords(training_data)
+
+# common.FrequencyDistribution(training_data['CleanedReview'])
+
+common.tf_idf(training_data)  # Uses previous bag of words to avoid duplication of process (resulting in poorer compuational performance)
 
 print(training_data)
 
--- src/common/visualisation.py
+++ src/common/visualisation.py
@@ -1,7 +1,37 @@
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+from os import environ
 
 # Space to visualise term frequency, etc.
 # compare negative vs positive sentiment in graph for basic data exploration/analysis
+
+# THIS DOESN'T WORK.... LOOK AT THIS LATER :/
+def ignore_qt_warnings():
+    '''
+    Ignores the QT Warnings when showing graphs using MatPlotLib or Seaborn
+    graphs on local machine
+
+    INSPIRATION: https://stackoverflow.com/questions/58194247/warning-qt-device-pixel-ratio-is-deprecated
+    
+    '''
+    environ["QT_DEVICE_PIXEL_RATIO"] = "0"
+    environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    environ["QT_SCREEN_SCALE_FACTORS"] = "1"
+    environ["QT_SCALE_FACTOR"] = "1"
+
+if __name__ == '__main__':
+    ignore_qt_warnings()
+
+def visualise_sentiment_type(dataframe):
+    '''Visualises the dataframe supplied and '''
+    ax = sns.catplot(data = dataframe, x = 'PositiveReview', kind = 'count', 
+                     palette='husl')
+    ax.set(xlabel = 'Review Sentiment', ylabel = 'Number of Reviews')
+    ax.set_xticklabels(['Negative', 'Positive'])
+    plt.show()
+
+
+
 
 
