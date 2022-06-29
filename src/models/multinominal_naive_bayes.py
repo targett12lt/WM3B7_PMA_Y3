1169218ca5d42782c8caed086a5ea9ec806ebab3
ltargett <ltargett@jaguarlandrover.com>
2022-06-29 11:38:44 +0100

    Starting development of Multinominal Naive Bayes Model

--- src/models/multinominal_naive_bayes.py
+++ src/models/multinominal_naive_bayes.py
@@ -1,13 +1,21 @@
-from sklearn.naive_bayes import (
-    BernoulliNB,  # For multivariate models and suiable for discrete data
-    ComplementNB,  # Used to correct 'severe assumptions made by Multinominal classifier
-    MultinomialNB,  # Suitable for classification with discrete features, can work with td-idf but made for integer counts ideally
-    CategoricalNB,  # For categorical geatures with discrete features
-)
+from sklearn.naive_bayes import MultinomialNB # Suitable for classification with discrete features, can work with td-idf but made for integer counts ideally
 
-classifiers = {
-    "MultinominmalNB": MultinomialNB(),
-    "BernoulliNB": BernoulliNB(),
-    "ComplementNB": ComplementNB(),
-    "CategoricalNB": CategoricalNB(),
-}
+def MultiNaiveBayes(TrainingData_X, TrainingData_Y, TestingData_X):
+    '''
+    Information here
+    
+    Inputs: 
+    * TrainingData_X - This is the 'feature engineered' training data to be used
+      to train the model.
+    * TrainingData_Y - This is the matching 'sentiment' values for the 'feature 
+      engineered' training data (TrainingData_X).
+    * TestingData_X - This is the 'feature engineered' testing data that you
+      want the model to predict based upon (i.e. the reviews to predict the 
+      sentiment)
+
+    Outputs: 
+    *
+
+    '''
+    
+
